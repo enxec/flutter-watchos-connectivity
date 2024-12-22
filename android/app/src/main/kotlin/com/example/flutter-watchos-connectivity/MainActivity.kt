631feabde5a7e0ba2b74db5cbff8ccf5e47fed70
이원모 <dev21mo@gmail.com>
2024-12-22 20:30:04 +0900

    예제 업로드

---
+++ android/app/src/main/kotlin/com/example/flutter-watchos-connectivity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter-watchos-connectivity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_watchos_connectivity/views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
